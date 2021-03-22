6f821de51a544570c7551df92357829cce18bea3
Justin696 <lee209@student.pcsstn.net>
2021-03-22 13:17:47 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,40 @@
+print("you can do all chalenges in this challenge")
+
+
+print("Please enter the Chalenge number from 1 to 7")
+num = int(input("<: "))
+
+
+if num < 1:
+  print("there are no challenges less than 1")
+
+elif num > 7:
+  print("There are no challenges more than 1")
+
+elif num == 1:
+  print("You have chosen to play chalenge 1 good luck")
+
+elif num == 2:
+  print("you have chosen to play chalenge 2 good luck")
+
+elif num == 3:
+  print("you have chosen to play chalenge 3 good luck")
+
+elif num == 4:
+  print("you have chosen to play chalenge 4 good luck")
+
+elif num == 5:
+  print("you have chosen to play chalenge 5 good luck")
+
+elif num == 6:
+  print("you have chosen to play chalenge 6 good luck")
+
+elif num == 7:
+  print("you have chosen to play chalenge 7 good luck")
+
+else:
+  print("what have you done")
+
+
+
+
